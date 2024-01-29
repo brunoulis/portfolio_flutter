54130fbc25c0b641180e2f904c37393c217be049
brunoulis <luisvapa2012@gmail.com>
2024-01-29 17:05:30 +0100

    Update color scheme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter/myHomePage.dart';
+import 'package:portfolio_flutter/themeprovider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MyApp(),
+    ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,36 +17,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Portfolio Bruno Vazquez',
+      themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          theme: ThemeData(
+            colorScheme: ColorScheme.light(), // Aquí puedes definir tu esquema de color claro
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.dark(), // Aquí puedes definir tu esquema de color oscuro
+          ),
       theme: ThemeData(
         useMaterial3: true,
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        // #23272a
+        
         colorScheme: const ColorScheme(
-          primary: Colors.white,
-          secondary: Colors.white,
-          surface: Color(0xFF23272A),
-          background: Colors.white,
-          error: Color(0xFF23272A),
-          onPrimary: Color(0xFF23272A),
-          onSecondary: Colors.white,
-          onSurface: Colors.white,
-          onBackground: Color(0xFF23272A),
-          onError: Color(0xFF23272A),
-          brightness: Brightness.light,
+          primary: Colors.white, // Color principal de la aplicación, usado en componentes como AppBar, FloatingActionButton, etc.
+          secondary: Colors.white, // Color secundario, usado en componentes secundarios y widgets interactivos.
+          surface: Color(0xFF23272A), // Color de elementos de superficie como Card, Dialog, Drawer, etc.
+          background: Color.fromARGB(255, 25, 27, 29), // Color de fondo de los componentes de la interfaz de usuario.
+          error: Color(0xFF23272A), // Color utilizado para indicar errores.
+          onPrimary: Color(0xFF23272A), // Color de un texto o icono cuando se coloca sobre un fondo primary.
+          onSecondary: Color(0xFF23272A), // Color de un texto o icono cuando se coloca sobre un fondo secondary.
+          onSurface: Colors.white, // Color de un texto o icono cuando se coloca sobre un fondo surface.
+          onBackground: Color.fromARGB(255, 224, 224, 224), // Color de un texto o icono cuando se coloca sobre un fondo background.
+          onError: Color(0xFF23272A), // Color de un texto o icono cuando se coloca sobre un fondo error.
+          brightness: Brightness.dark, // Define si el tema es claro u oscuro. En este caso, es Brightness.light, lo que significa que el tema es claro.
         ),
       ),
       home: const MyHomePage(title: 'Portfolio Bruno Vazquez'),
